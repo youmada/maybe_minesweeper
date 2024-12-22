fc80e9711fa32887b595fb31a8126b7d126a91f8
youmada <117708788+youmada@users.noreply.github.com>
2024-12-22 12:21:08 +0900

    Revert "マインスイーパーのコア機能のテストを作成"

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
-// ホーム画面ルート
 Route::get('/', function () {
-    return Inertia::render('Home');
-});
-
-// シングルプレイ設定画面ルート
-Route::get('/single', function () {
-    return Inertia::render('Single');
-});
-
-Route::get('/single/play', function () {
-    return Inertia::render('Single/Play', [
-        'level' => request()->query('level'),// levelにマインスイーパーの難易度を入れる
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
@@ -32,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
