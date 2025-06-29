4a2aa6fe344e3ebb9c9caaea3c8ee47de59337e9
youma <yo65705@gmail.com>
2025-06-29 14:36:07 +0900

    追加： フロントエンド： ・マルチプレイ初期画面の仮デザイン完成

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 <template>
     <button
         @click="() => clickFn && clickFn()"
-        class="inline-flex items-center rounded-md border border-transparent text-gray-800 bg-gray-200 px-4 py-2 text-lg font-semibold uppercase tracking-widest transition duration-150 ease-in-out  hover:bg-white"
+        class="inline-flex items-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-lg font-semibold uppercase tracking-widest text-gray-800 transition duration-150 ease-in-out hover:bg-white"
     >
         <slot />
     </button>
