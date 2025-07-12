9d78d2c00ad77e9ebcf2b818fdbd24a58cf4b7ee
youma <yo65705@gmail.com>
2025-07-12 21:58:52 +0900

    WIP

---
+++ resources/js/Composables/useRoomData.ts
@@ -0,0 +1,29 @@
+import { useEcho } from '@laravel/echo-vue';
+import { computed, ref } from 'vue';
+
+type RoomData = {
+    status: string;
+};
+
+export function useRoomData(roomPublicId: string) {
+    const { channel } = useEcho(`room.${roomPublicId}.data`, ['FetchRoomData']);
+    const roomState = ref<RoomData>({
+        status: '',
+    });
+    channel().listen('FetchRoomData', (data: RoomData) => {
+        console.log(data);
+        roomState.value = data;
+    });
+
+    const isRoomReady = computed(() => {
+        return (
+            roomState.value.status === 'playing' ||
+            roomState.value.status === 'standby'
+        );
+    });
+
+    return {
+        roomState,
+        isRoomReady,
+    };
+}
