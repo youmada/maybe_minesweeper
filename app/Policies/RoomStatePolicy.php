4b345941ead1d30d95785dd63e6107bcaeeaa104
youma <yo65705@gmail.com>
2025-07-23 13:16:44 +0900

    追加： バックエンド： ・コントローラにポリシーを実装した。

--- app/Policies/RoomStatePolicy.php
+++ app/Policies/RoomStatePolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Domain\Room\RoomAggregate;
 use App\Domain\Room\RoomState;
+use App\Domain\Room\RoomStatus;
 use App\Models\Player;
 // use App\Models\RoomState;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -12,27 +13,36 @@ class RoomStatePolicy
 {
     use HandlesAuthorization;
 
-    //    public function viewAny(Player $user): bool {}
-
-    //    public function view(Player $user, RoomState $roomState): bool {}
-
-    //    public function create(Player $user): bool {}
+    //    public function view (Player $user, RoomState $roomState): bool {}
 
     public function update(Player $user, RoomAggregate $roomAggregate): bool
     {
-        if ($roomAggregate->getRoomStatus() === 'finished') {
+        if ($roomAggregate->getRoomStatus() === RoomStatus::FINISHED->value) {
             return false;
         }
-        if ($roomAggregate->getRoomStatus() === 'waiting') {
+        if ($roomAggregate->getRoomStatus() === RoomStatus::WAITING->value) {
             return false;
         }
 
         return $user->public_id === $roomAggregate->getCurrentOrder();
     }
 
-    //    public function delete(Player $user, RoomState $roomState): bool {}
+    //    public function delete (Player $user, RoomState $roomState): bool {}
 
-    //    public function restore(Player $user, RoomState $roomState): bool {}
+    public function restore(Player $user, RoomAggregate $roomAggregate): bool
+    {
+        return match ($roomAggregate->getRoomStatus()) {
+            RoomStatus::PLAYING->value, RoomStatus::FINISHED->value, RoomStatus::STANDBY->value => false,
+            default => true,
+        };
+    }
 
-    //    public function forceDelete(Player $user, RoomState $roomState): bool {}
+    public function continueAction(Player $user, RoomAggregate $roomAggregate): bool
+    {
+        if ($roomAggregate->getRoomStatus() === RoomStatus::FINISHED->value) {
+            return true;
+        }
+
+        return false;
+    }
 }
